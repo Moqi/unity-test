5cb0c639690bf5b070cfcb9e8713bffd393ef4ec
Mincl <eonchan99@gmail.com>
2014-08-26 00:30:14 +0900

    first commit

---
+++ Assets/ArmyScript.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+//var groundPlane:Plane;
+
+private var smooth:int;
+private var targetPosition:Vector3;
+
+function Start () {
+
+}
+
+function Update () {
+	transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime * smooth);
+}
+
+public function setProperty(smth: int, targetPos: Vector3) {
+	smooth = smth;
+	targetPosition = targetPos;
+}
---
+++ Assets/MoveManager.js
@@ -0,0 +1,49 @@
+﻿#pragma strict
+
+public var smooth:int;
+private var selectedObj:GameObject;
+private var isSelected:boolean;
+
+function Start () {
+}
+
+function Update () {
+	if(Input.GetMouseButtonDown(0)) {
+		// catch click event
+		var hitInfo:RaycastHit;
+		var hit = Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), hitInfo);
+		
+		Debug.Log("click event");
+		if(hit) {
+			// hit some object
+			var hitObj = hitInfo.transform.gameObject;
+			Debug.Log("hit this obj : " + hitObj.name);
+			if(hitObj.tag == "Armys") {
+				// select "Army" object
+				Debug.Log("select army");
+				selectedObj = hitObj;
+				isSelected = true;
+			} else if(hitObj.tag == "Grounds" && isSelected) {
+				// move selected "Army" object
+				Debug.Log("select ground and move");
+				var hitPlane = new Plane(Vector3.up, selectedObj.transform.position);
+				var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				var hitdist = 0.0;
+				
+				if (hitPlane.Raycast(ray, hitdist)) {
+					var targetPoint = ray.GetPoint(hitdist);
+					selectedObj.GetComponent(ArmyScript).setProperty(smooth, ray.GetPoint(hitdist));
+					var targetRotation = Quaternion.LookRotation(targetPoint - selectedObj.transform.position);
+					selectedObj.transform.rotation = targetRotation;
+				}
+			} else {
+				// unselect "Army" Object
+				Debug.Log("select else");
+				isSelected = false;
+			}
+		} else {
+			// if not hit, then unselect "Army" Object
+			isSelected = false;
+		}
+	}
+}
